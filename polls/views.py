bafe45c4d93c88a4ac3f5a6e244b3c3499b42b13
Monorail3224 <callen_12@mail.fhsu.edu>
2023-11-05 11:53:17 -0600

    Merge branch 'In_Progress' of https://github.com/Monorail3224/miniproject4CA into In_Progress

--- polls/views.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.poll_list, name='poll_list'),
-    path('<int:poll_id>/', views.poll_detail, name='poll_detail'),
-    path('create/', views.poll_create, name='poll_create'),
-    path('<int:poll_id>/vote/', views.poll_vote, name='poll_vote'),
-]
