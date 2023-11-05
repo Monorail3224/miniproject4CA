bb8eb473acb426f14e58dc7a9ca48c68a84eef0e
Monorail3224 <callen_12@mail.fhsu.edu>
2023-11-05 12:55:13 -0600

    modified urls.py to reflect

---
+++ polls/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import PollListView, PollDetailView, PollCreateView, PollEditView
+
+app_name = 'polls'
+
+urlpatterns = [
+    path('', PollListView.as_view(), name='poll_list'),
+    path('<int:poll_id>/', PollDetailView.as_view(), name='poll_detail'),
+    path('create/', PollCreateView.as_view(), name='poll_create'),
+    path('<int:poll_id>/edit/', PollEditView.as_view(), name='poll_edit'),
+]
